00fad1d28a5ca1051dc0bc186830b418791d519b
bantarou <takowasakun@gmail.com>
2017-10-02 01:19:15 +0900

    マス目の描画を実装

--- src/constants.py
+++ src/constants.py
@@ -11,6 +11,17 @@ class constans:
   WINDOW_MARGIN_HEIGHT = 25
 
   #
+  # ピクロスのマス目の描画　に関係する定数
+  #
+
+  #普通の線の太さ
+  NORMAL_LINE_SIZE = 0.5
+  #太線の太さ
+  BOLD_LINE_SIZE = 1.5
+  #太線を引く間隔
+  BOLD_LINE_SPAN = 5
+
+  #
   # 数字の表示に関係する定数
   #
 
--- src/picross_graphic.py
+++ src/picross_graphic.py
@@ -8,13 +8,31 @@ from constants import constans as co
 from picross_processing import *
 
 #ドットを描画する関数
-def draw_dot(canvas, img, box_size, hint_width, hint_height):
+def draw_pic_dot(canvas, img, box_size, hint_width, hint_height):
   for y in range(0, len(img)):
     for x in range(0, len(img[0])):
       if img[y][x] == 0:
         piv_x = hint_width + x * box_size
         piv_y = hint_height + y * box_size
         canvas.create_rectangle(piv_x, piv_y, piv_x + box_size, piv_y + box_size, fill = 'black')
+
+#線を描画する関数
+def draw_pic_line(canvas, img, box_size, hint_width, hint_height):
+  row_length = hint_width + len(img[0]) * box_size
+  column_length = hint_height + len(img) * box_size
+  #横の線を描画
+  for y in range(0, len(img) + 1):
+    if y % co.BOLD_LINE_SPAN == 0:
+      canvas.create_line(0, hint_height + y * box_size, row_length, hint_height + y * box_size, width = co.BOLD_LINE_SIZE)
+    else:
+      canvas.create_line(0, hint_height + y * box_size, row_length, hint_height + y * box_size, width = co.NORMAL_LINE_SIZE)
+
+  #縦の線を描画
+  for x in range(0, len(img[0]) + 1):
+    if x % co.BOLD_LINE_SPAN == 0:
+      canvas.create_line(hint_width + x * box_size, 0, hint_width + x * box_size, column_length, width = co.BOLD_LINE_SIZE)
+    else:
+      canvas.create_line(hint_width + x * box_size, 0, hint_width + x * box_size, column_length, width = co.NORMAL_LINE_SIZE)
 
 #GUIの制御部分
 def draw_main(img, box_size):
@@ -41,7 +59,8 @@ def draw_main(img, box_size):
   canvas = tkinter.Canvas(root, width = pic_width, height = pic_height)
 
   #ピクロスのドット部を描画
-  draw_dot(canvas, img, box_size, hint_width, hint_height)
+  draw_pic_dot(canvas, img, box_size, hint_width, hint_height)
+  draw_pic_line(canvas, img, box_size, hint_width, hint_height)
 
   #キャンバスバインド
   canvas.place(x=co.WINDOW_MARGIN_WIDTH, y=co.WINDOW_MARGIN_HEIGHT)
